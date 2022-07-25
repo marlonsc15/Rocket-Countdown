39b700ff0d86ec611afa77fb4cbee8cc94def31d
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-07-25 16:41:23 -0300

    adição do popUp e sua funcionalidade

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 const countdown = () => {
-    const countDate = new Date("jully 30, 2022 00:00:00").getTime();
+    const countDate = new Date("jully 25, 2022 16:37:40").getTime();
     const now = new Date().getTime();
     const gap = countDate - now; 
-
+    
 
 const second = 1000;
 const minute = second * 60;
@@ -15,11 +15,28 @@ const texMinute = Math.floor((gap % hour) / minute);
 const textSecond = Math.floor((gap % minute)/ second);
 
 document.querySelector(".day").innerHTML = textDay;
-document.querySelector(".hour").innerHTML = textHour;
-document.querySelector(".minute").innerHTML = texMinute;
-document.querySelector(".second").innerHTML = textSecond;
+document.querySelector(".hour").innerHTML = ": "+textHour;
+document.querySelector(".minute").innerHTML = ": "+texMinute;
+document.querySelector(".second").innerHTML = ": "+textSecond;
 
 
+function decolar(){
+    remove(".rocket");
+}
+if(gap > countDate)
+       decolar();
 };
 
+
+
 setInterval(countdown, 1000);
+
+const popup = document.getElementById("popup");
+
+function openPopup() {
+    popup.classList.add("open-popup")
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
