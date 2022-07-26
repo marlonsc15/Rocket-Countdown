e1a73b61f48113d48ddac75e662a512ea311878d
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-07-26 12:43:04 -0300

    incremento na tag input

--- java.js
+++ java.js
@@ -1,29 +1,26 @@
 const countdown = () => {
     const countDate = new Date("jully 30, 2022 19:00:00").getTime();
     const now = new Date().getTime();
-    const gap = countDate - now; 
-    
+    const gap = countDate - now;    
 
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-const textDay = Math.floor(gap / day);
-const textHour = Math.floor((gap % day) / hour);
-const texMinute = Math.floor((gap % hour) / minute);
-const textSecond = Math.floor((gap % minute)/ second);
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const texMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute)/ second);
 
-document.querySelector(".day").innerHTML = textDay;
-document.querySelector(".hour").innerHTML = ": "+textHour;
-document.querySelector(".minute").innerHTML = ": "+texMinute;
-document.querySelector(".second").innerHTML = ": "+textSecond;
+    document.querySelector(".day").innerHTML = textDay;
+    document.querySelector(".hour").innerHTML = ": "+textHour;
+    document.querySelector(".minute").innerHTML = ": "+texMinute;
+    document.querySelector(".second").innerHTML = ": "+textSecond;
     
 };
 
 //rocket
-
-
 
 function decolar(){
     const rocket = document.getElementById("rocket");
